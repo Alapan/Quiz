afa28986db23b033ba39fe7650e2208a2b86c0c2
Alapan Mukherjee <alapan.mukherjee35@gmail.com>
2025-09-24 16:27:23 +0100

    Add project files.

---
+++ Quiz/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("*******************************");
+Console.WriteLine("********** Quiz Time **********");
+Console.WriteLine("*******************************");
+
+Console.WriteLine("Select your user type from the options below: ");
+Console.WriteLine("1: Admin");
+Console.WriteLine("2: Player");
+
+int userType = int.Parse(Console.ReadLine());
+Console.WriteLine("The user type is " +  userType);
