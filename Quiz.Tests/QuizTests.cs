fed8a57eff10b682afaf2dc37489fc0b28ef78be
Alapan Mukherjee <alapan.mukherjee35@gmail.com>
2025-10-26 20:48:29 +0000

    Refs #3 Save quiz data to JSON file

---
+++ Quiz.Tests/QuizTests.cs
@@ -0,0 +1,21 @@
+﻿using QuizGame;
+
+namespace Quiz.Tests
+{
+    public class QuizTests
+    {
+        [Fact]
+        public void Quiz_SaveToJson_CreatesFile()
+        {
+            int quizId = 1;
+            Entry entry1 = new Entry("What is 2+2", new string[] { "4", "5", "6", "7" }, "4");
+            Entry entry2 = new Entry("What is the capital of France?", new string[] {"Berlin", "London", "Paris", "Rome" }, "Paris");
+            List<Entry> entries = new List<Entry> { entry1, entry2 };
+            QuizGame.Quiz quiz = new QuizGame.Quiz(quizId, entries);
+            quiz.saveToJson();
+            Directory.CreateDirectory("Quizzes");
+            Assert.True(File.Exists(Path.Combine("Quizzes", $"{quizId}.json")));
+            File.Delete(Path.Combine("Quizzes", $"{quizId}.json"));
+        }
+    }
+}
